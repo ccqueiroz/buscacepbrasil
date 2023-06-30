1d210530be44c1e471abc07a290aa7abb8c324a4
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-06-30 15:13:25 -0300

    feat: create file to export app server

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,19 @@
-import * as functions from 'firebase-functions';
-import admin from 'firebase-admin';
+import admin, { ServiceAccount } from 'firebase-admin';
 import express from 'express';
 import cors from 'cors';
 import serviceAccount from '../serviceAccountKey.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount as ServiceAccount),
 });
 
 const app = express();
 
 app.use(cors({ origin: true }));
-const db = admin.firestore();
 
-exports.app = functions.https.onRequest(app);
+app.get('/', (request, response) => {
+  console.log('request', request);
+  return response.status(200).send('hello cambada');
+});
+
+export { app };
