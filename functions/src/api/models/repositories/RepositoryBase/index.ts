d47c109cc9938ba178954d20d77badff69f8785e
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-07-13 12:05:00 -0300

    refactor: exclude abstract class Repository Base

--- functions/src/api/models/repositories/RepositoryBase/index.ts
+++
@@ -1,9 +0,0 @@
-import { cepRegionsByFirstDigitCep } from '../../cepRegions/index';
-import { CepInterface } from '../../cep';
-
-export abstract class RepositoryBase {
-  abstract getCep(
-    region: (typeof cepRegionsByFirstDigitCep)[keyof typeof cepRegionsByFirstDigitCep],
-    cep: string,
-  ): Promise<CepInterface | Error>;
-}
