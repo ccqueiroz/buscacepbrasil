f53c68316c8b7189ad65991e14e2ee8e369c9d1f
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-07-13 18:09:33 -0300

    refactor: controller getCep

--- functions/src/api/controllers/getCep/index.ts
+++ functions/src/api/controllers/getCep/index.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from 'express';
+import { ServiceGetCep } from '../../services/getCep';
+import { RepositoryBase } from '../../repositories/RepositoryBase';
+import { FireBaseRepository } from '../../repositories/FirebaseRepository';
+import { clientFirebase } from '../../lib/clients/firebase';
+import { FireBaseError } from '../../errors/FireBaseError';
 
 export const controllerGetCep = async (
   request: Request,
   response: Response,
 ) => {
-  const { params, ip } = request;
+  const { params } = request;
   const { cep } = params;
+  const fireBaseRepository = new FireBaseRepository({
+    client: clientFirebase,
+    firebaseError: FireBaseError.init(),
+  });
 
-  return response.send({ cep, ip });
+  const repositoryBase = new RepositoryBase(fireBaseRepository);
+  const serviceGetCep = new ServiceGetCep(repositoryBase, cep);
+
+  const responseCep = await serviceGetCep.getCep();
+
+  return response.send(responseCep);
 };
