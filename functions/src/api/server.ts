d836854a6b318b2cea9f292505baccc2006530c5
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-07-17 17:18:17 -0300

    chore: include middleware rate limit

--- functions/src/api/server.ts
+++ functions/src/api/server.ts
@@ -1,12 +1,15 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import { routes } from './routes';
+import { middlewareRateLimit } from './middlewares/rateLimit';
 
 const app = express();
 
 app.set('x-powered-by', false);
 
 app.use(cors({ origin: true }));
+
+app.use(middlewareRateLimit);
 
 app.use(json());
 
