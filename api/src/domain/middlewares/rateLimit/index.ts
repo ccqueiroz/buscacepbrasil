10c52a0519828d0164d53543c7977bb5913231b8
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-07-27 11:27:49 -0300

    chore: move middlewares to domain

---
+++ api/src/domain/middlewares/rateLimit/index.ts
@@ -0,0 +1,35 @@
+import { AbstractRepositoryBaseCache } from '../../models/repositories/AbstractRepositoryBaseCache';
+import { ResponseAdapter } from '../../models/responseAdapter/responseHTTPAdapter';
+import { ServiceCacheRateLimit } from '../../services/cacheRateLimit';
+import {
+  RequestInfra,
+  ResponseInfra,
+  NextFunctionInfra,
+} from '../../../infra/models/http';
+
+export class MiddlewareRateLimit {
+  private serviceCacheRateLimit: ServiceCacheRateLimit;
+
+  constructor(baseCacheRepository: AbstractRepositoryBaseCache) {
+    this.serviceCacheRateLimit = new ServiceCacheRateLimit(baseCacheRepository);
+  }
+
+  public async execute(
+    request: RequestInfra,
+    response: ResponseInfra,
+    next: NextFunctionInfra,
+  ) {
+    const { ip } = request;
+
+    const responseRateLimit = await this.serviceCacheRateLimit.rateLimit(ip);
+
+    if (responseRateLimit.code !== 200) {
+      return response.status(responseRateLimit.code).send({
+        error: responseRateLimit.message,
+        success: false,
+      } as ResponseAdapter);
+    }
+
+    return next();
+  }
+}
