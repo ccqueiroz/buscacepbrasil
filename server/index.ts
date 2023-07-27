a8cb7410c60d14eb10ee6de055f71b6d811be14b
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-07-27 11:33:57 -0300

    chore: add BootStrap

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,23 @@
-import { app } from '../api/index';
+import { FireBaseRepository } from '../api/src/infra/repositories/FirebaseRepository/index';
+import { RedisRepository } from '../api/src/infra/repositories/RedisRepository';
+import { FireBaseError } from '../api/src/infra/errors/FireBaseError';
+import { clientFirebase } from '../api/src/infra//lib/clients/firebase'
+import { clientRedis } from '../api/src/infra//lib/clients/redis'
+import { BootStrap } from '../api/index'
+
+const fireBaseRepository = new FireBaseRepository({
+    client: clientFirebase,
+    firebaseError: FireBaseError.init(),
+});
+
+const redisRepository = new RedisRepository({
+    client: clientRedis,
+});
+
+const app = new BootStrap({
+    providerRepository: fireBaseRepository,
+    providerRepositoryCache: redisRepository
+});
 
 const _PORT = 8888;
 
